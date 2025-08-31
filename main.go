35e3d7cc1f918cd2d0eb867f5844348b0f1da044
IkhwanAl <ikhwanal235@gmail.com>
2025-08-31 10:33:08 +0700

    feat: Init

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/ikhwanal/pixel_art_scaler/page"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		main := page.WebHtml()
+
+		if err := main.Render(r.Context(), w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	log.Println("listening on http://localhost:8080")
+	if err := http.ListenAndServe("127.0.0.1:8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
