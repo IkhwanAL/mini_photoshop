224055c042d69bc195d346286212baaeffd8b4ca
IkhwanAl <ikhwanal235@gmail.com>
2025-09-09 15:23:05 +0700

    feat: add filter and dynamic slider for brightness and contrast

---
+++ assets/js/control_option.js
@@ -0,0 +1,27 @@
+				<span id="brightnessValue">0</span>
+				<span id="brightnessValue">0</span>
+const filterOption = document.getElementById("filter")
+const compBrightnessContrast = document.getElementById("brightnessContrast")
+
+filterOption.addEventListener("change", (ev) => {
+	if (ev.target.value == "brightness-contrast") {
+		compBrightnessContrast.classList.remove("hidden")
+	}else{
+		compBrightnessContrast.classList.add("hidden")
+	}
+	
+})
+
+const brightness = document.getElementById("brightness")
+const brightnessValue = document.getElementById("brightnessValue")
+
+brightness.addEventListener("input", ev => {
+	brightnessValue.textContent = ev.target.value
+})
+
+const contrast = document.getElementById("contrast")
+const contrastValue = document.getElementById("contrastValue")
+
+contrast.addEventListener("input", ev => {
+	contrastValue.textContent = ev.target.value
+})
